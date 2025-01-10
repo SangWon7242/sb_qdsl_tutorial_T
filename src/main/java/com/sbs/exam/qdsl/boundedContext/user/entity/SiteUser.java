8271f373786320b3b57cef36ad9cb28e20ba7e65
SangWon7242 <gotkdqja@gmail.com>
2025-01-10 10:47:56 +0900

    UserRepository 추가, 회원 추가 테스트 케이스

--- src/main/java/com/sbs/exam/qdsl/boundedContext/user/entity/SiteUser.java
+++ src/main/java/com/sbs/exam/qdsl/boundedContext/user/entity/SiteUser.java
@@ -1,17 +1,22 @@
 package com.sbs.exam.qdsl.boundedContext.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
 public class SiteUser {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(unique = true)
+  private String username;
 
   private String password;
 
---
+++ src/main/java/com/sbs/exam/qdsl/boundedContext/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.sbs.exam.qdsl.boundedContext.user.repository;
+
+import com.sbs.exam.qdsl.boundedContext.user.entity.SiteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
