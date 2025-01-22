ddfce49c4bc5070dba7305a6585c875a0cda5d23
SangWon7242 <gotkdqja@gmail.com>
2025-01-23 02:17:50 +0900

    테스트 케이스 구현

--- src/main/java/com/sbs/exam/qdsl/boundedContext/user/entity/SiteUser.java
+++ src/main/java/com/sbs/exam/qdsl/boundedContext/user/entity/SiteUser.java
@@ -35,6 +35,10 @@ public class SiteUser {
   @ManyToMany(cascade = CascadeType.ALL)
   private Set<SiteUser> followers = new HashSet<>();
 
+  @Builder.Default
+  @ManyToMany(cascade = CascadeType.ALL)
+  private Set<SiteUser> followings = new HashSet<>();
+
   public void addInterestKeywordContent(String keywordContent) {
     interestKeywords.add(new InterestKeyword(keywordContent));
   }
@@ -44,7 +48,11 @@ public class SiteUser {
     if (following == null) return;
     if (this.getId() == following.getId()) return;
 
+    // 유튜버(following)이 나(follower)를 구독자로 등록
     following.getFollowers().add(this);
+
+    // 내(follower)가 유튜버(following)를 구독한다.
+    getFollowings().add(following);
   }
 
   public Set<Object> getFollowings() {
