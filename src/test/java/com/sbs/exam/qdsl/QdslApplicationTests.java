265fd7998638edcded6d88934b1740037fe9f510
SangWon7242 <gotkdqja@gmail.com>
2025-01-25 01:54:56 +0900

    테스트용 회원을 8명으로 늘리기

--- src/test/java/com/sbs/exam/qdsl/QdslApplicationTests.java
+++ src/test/java/com/sbs/exam/qdsl/QdslApplicationTests.java
@@ -33,15 +33,15 @@ class QdslApplicationTests {
 	@DisplayName("회원 생성")
 	void t1() {
 		SiteUser u3 = SiteUser.builder()
-				.username("user3")
+				.username("user9")
 				.password("{noop}1234")
-				.email("user3@test.com")
+				.email("user9@test.com")
 				.build();
 
 		SiteUser u4 = SiteUser.builder()
-				.username("user4")
+				.username("user10")
 				.password("{noop}1234")
-				.email("user4@test.com")
+				.email("user10@test.com")
 				.build();
 
 		userRepository.saveAll(Arrays.asList(u3, u4));
@@ -203,9 +203,9 @@ class QdslApplicationTests {
 		assertThat(users.size()).isEqualTo(pageSize);
 
 		SiteUser u = users.get(0);
-		assertThat(u.getId()).isEqualTo(1L);
-		assertThat(u.getUsername()).isEqualTo("user1");
-		assertThat(u.getEmail()).isEqualTo("user1@test.com");
+		assertThat(u.getId()).isEqualTo(7L);
+		assertThat(u.getUsername()).isEqualTo("user7");
+		assertThat(u.getEmail()).isEqualTo("user7@test.com");
 		assertThat(u.getPassword()).isEqualTo("{noop}1234");
 	}
 
